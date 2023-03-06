31b38c2f5ddf7a63783d10701f4cd7f005043ad6
2maeng <vbn3701@gmail.com>
2023-03-06 19:24:48 +0900

    반복문

---
+++ MYAPP/break.js
@@ -0,0 +1,48 @@
+// 08-18
+// if (true) {
+//     break; // Uncaught SyntaxError: Illegal break statement
+// }
+
+// 08-19
+// foo라는 레이블 식별자가 붙은 레이블 문
+foo: console.log('foo');
+
+// 08-20
+// foo라는 식별자가 붙은 레이블 반복문
+foo: {
+  console.log(1);
+  break foo; // foo 레이블 블록문을 탈출한다.
+  console.log(2);
+}
+
+console.log('Done');
+
+// 08-21
+// outer라는 식별자가 붙은 레이블 for 문
+outer: for (var i = 0; i < 3; i++) {
+  for (var j = 0; j < 3; j++) {
+    // i + j === 3이면 outer라는 식별자가 붙은 레이블 for 문을 탈출한다.
+    if (i + j === 3) break outer;
+    console.log(`inner [${i}, ${j}]`);
+  }
+}
+
+console.log('Done!');
+
+// 08-22
+var string = 'Hello World';
+var search = 'l';
+var index;
+
+// 문자열은 유사 배열이므로 for 문으로 순화할 수 있다.
+for (var i = 0; i < string.length; i++) {
+    // 문자열의 개별 문자가 'l'이면
+    if (string[i] === search) {
+        index = i;
+        break; // 반복문을 탈출한다.
+    }
+}
+
+console.log(index); // 2
+// 참고로 String.prototype.indexOf 메서드를 사용해도 같은 동작을 한다.
+console.log(string.indexOf(search)); // 2
---
+++ MYAPP/switch.js
@@ -0,0 +1,107 @@
+// 08-07
+// 월을 영어로 변환한다. (11 -> 'November')
+var month = 11;
+var monthName;
+
+switch (month) {
+  case 1:
+    monthName = 'January';
+  case 2:
+    monthName = 'February';
+  case 3:
+    monthName = 'March';
+  case 4:
+    monthName = 'April';
+  case 5:
+    monthName = 'May';
+  case 6:
+    monthName = 'June';
+  case 7:
+    monthName = 'July';
+  case 8:
+    monthName = 'August';
+  case 9:
+    monthName = 'September';
+  case 10:
+    monthName = 'October';
+  case 11:
+    monthName = 'November';
+  case 12:
+    monthName = 'December';
+  default:
+    monthName = 'Invalid month';
+}
+
+console.log(monthName); // Invalid month
+
+// 08-08
+// 월을 영어로 변환한다. (11 -> 'November')
+var month = 11;
+var monthName;
+
+switch (month) {
+  case 1:
+    monthName = 'January';
+    break;
+  case 2:
+    monthName = 'February';
+    break;
+  case 3:
+    monthName = 'March';
+    break;
+  case 4:
+    monthName = 'April';
+    break;
+  case 5:
+    monthName = 'May';
+    break;
+  case 6:
+    monthName = 'June';
+    break;
+  case 7:
+    monthName = 'July';
+    break;
+  case 8:
+    monthName = 'August';
+    break;
+  case 9:
+    monthName = 'September';
+    break;
+  case 10:
+    monthName = 'October';
+    break;
+  case 11:
+    monthName = 'November';
+    break;
+  case 12:
+    monthName = 'December';
+    break;
+  default:
+    monthName = 'Invalid month';
+}
+
+console.log(monthName); // November 
+
+// 08-09
+var year = 2000; // 2000년은 윤년으로 2월이 29일이다.
+var month = 2;
+var days = 0;
+
+switch (month) {
+    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        days = 31;
+        break;
+    case 4: case 6: case 9: case 11:
+        days = 30;
+    case 2:
+        // 윤년 계산 알고리즘
+        // 1. 연도가 4로 나누어떨어지는 해(2000, 2004, 2008, 2012, 2016, 2020...)는 윤년이다.
+        // 2. 연도가 4로 나누어떨어지더라도 연도가 나누어떨어지는 해 (2000, 2100, 2200...)는 평년이다.
+        // 3. 연도가 400으로 나누누어떨어지는 해(2000, 2400, 2800...)는 윤년이다.
+        days = ((year % 4 ===3 && year % 100 !== 0) || (year % 400 === 0)) ? 29 : 28;
+        break;
+    default:
+        console.log('Invalid month'); 
+}
+
+console.log(days); // 29
